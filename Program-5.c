a9fd39cc7bf34392f4fbdee7b4937ce4fc1e7935
nandhitha1326 <Nandhithak13@gmail.com>
2021-03-24 13:17:58 +0000

    Done

--- Program-5.c
+++ Program-5.c
@@ -1,8 +1,9 @@
 //INPUT: 5 7
 //OUTPUT: 7 13    6 14
-
+// insert the missing line
+ int main ()
 { 
-     // insert #inthe missing code
+     // insert the missing code
 printf("Enter the values of i and j");
 scanf("%d%d",&i,&j);
      i = i + 1;
